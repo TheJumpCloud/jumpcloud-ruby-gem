a7e616a49b02bf8fd773a95022cebd232b590ba4
Topher Marie <topherMarie@gmail.com>
2014-05-17 17:52:36 -0600

    This gem does a poor man's patch on setting the name and the tags

--- lib/jumpcloud.rb
+++ lib/jumpcloud.rb
@@ -5,53 +5,65 @@ require 'net/http'
 
 class JumpCloud
   def self.get_date
-    now = Time.now.utc.strftime("+%a, %d %h %Y %H:%M:%S GMT")
+    Time.now.utc.strftime("+%a, %d %h %Y %H:%M:%S GMT")
   end
 
   def self.parse_config
-    jc_conf = JSON.parse( IO.read("/opt/jc/jcagent.conf") )
+    JSON.parse( IO.read("/opt/jc/jcagent.conf") )
   end
 
   def self.get_key_from_config
-    jc_conf = parse_config["systemKey"]
+    parse_config["systemKey"]
   end
 
-  def self.create_signature(date, system_key)
-    signed_string = "PUT /api/systems/#{system_key} HTTP/1.1\ndate: #{date}"
+  def self.create_signature(verb, date, system_key)
+    signed_string = "#{verb} /api/systems/#{system_key} HTTP/1.1\ndate: #{date}"
     key = OpenSSL::PKey::RSA.new(File.open("/opt/jc/client.key"))
     Base64.strict_encode64(key.sign(OpenSSL::Digest::SHA256.new, signed_string))
   end
 
   def self.set_system_tags(*tags)
-    tags_list = tags.join(%(\",\"))
-    data = %({\"tags\" : [\"#{tags_list}\"]})
-    post_to_server(data)
-  end
-
-
-  def self.set_system_tag(tag_id)
-    data = %({\"tags\" : [\"#{tag_id}\"]})
-    post_to_server(data)
+    system_data = get_system_data()
+    system_data["tags"] = tags
+    send_to_server(system_data)
   end
 
   def self.set_system_name(system_name)
-    data = %({\"displayName\" : \"#{system_name}\"})
-    post_to_server(data)
+    system_data = get_system_data()
+    system_data["displayName"] = system_name
+    send_to_server(system_data)
   end
 
-  #This could now take json
-  def self.post_to_server(data)
+  def self.get_system_data()
     date = get_date
     system_key = get_key_from_config
-    signature = create_signature(date, system_key)
+    signature = create_signature("GET", date, system_key)
+    uri = URI("https://console.jumpcloud.com/api/systems/#{system_key}")
+    request = Net::HTTP::Get.new(uri)
+    #request.set_content_type("application/json")
+    request.add_field("Authorization", "Signature keyId=\"system/#{system_key}\",headers=\"request-line date\",algorithm=\"rsa-sha256\",signature=\"#{signature}\"")
+    request.add_field("Date", "#{date}")
+    request.add_field("accept", "application/json")
+    Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http.ssl_version = :SSLv3
+      response = http.request(request)
+      return JSON.parse(response.body)
+    end
+  end
+
+  def self.send_to_server(data)
+    date = get_date
+    system_key = get_key_from_config
+    signature = create_signature("PUT", date, system_key)
     uri = URI("https://console.jumpcloud.com/api/systems/#{system_key}")
     request = Net::HTTP::Put.new(uri)
     request.set_content_type("application/json")
     request.add_field("Authorization", "Signature keyId=\"system/#{system_key}\",headers=\"request-line date\",algorithm=\"rsa-sha256\",signature=\"#{signature}\"")
     request.add_field("Date", "#{date}")
     request.add_field("accept", "application/json")
-    request.body = data
-    result = Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
+    request.body = JSON.generate(data)
+    Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       http.ssl_version = :SSLv3
       http.request(request)
