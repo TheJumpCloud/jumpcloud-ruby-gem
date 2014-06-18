9278a18bb23ddbf6d8c6576c0725a1b6615968f8
jbrown <james@jumpcloud.com>
2014-06-18 11:48:56 -0600

    Added delete_system() function to allow a system to delete itself from JumpCloud.

--- lib/jumpcloud.rb
+++ lib/jumpcloud.rb
@@ -34,6 +34,23 @@ class JumpCloud
     send_to_server(system_data)
   end
 
+  def self.delete_system()
+    date = get_date
+    system_key = get_key_from_config
+    signature = create_signature("DELETE", date, system_key)
+    uri = URI.parse("https://console.jumpcloud.com/api/systems/#{system_key}")
+    request = Net::HTTP::Delete.new(uri.request_uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    http.ssl_version = :SSLv3
+    request["Authorization"] = "Signature keyId=\"system/#{system_key}\",headers=\"request-line date\",algorithm=\"rsa-sha256\",signature=\"#{signature}\""
+    request["Date"] = "#{date}"
+    request["accept"] = "application/json"
+    request["Content-Type"] = "application/json"
+    response = http.request(request)
+  end
+
   def self.get_system_data()
     date = get_date
     system_key = get_key_from_config
